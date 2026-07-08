b506ee5218ef8f6b165ca4242c3f937665e6d84d
nitinkodam11 <nitin.kodam11@gmail.com>
2026-07-08 09:27:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "name" {
+    ami = "ami-01a00762f46d584a1"
+    instance_type = "t3.micro"
+
+    tags = {
+      Name =  "prod"
+    }
+}
